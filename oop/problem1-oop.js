f7e1fde2e705cb35933aa7cd148c1e967cf4747f
Delilah Lopez <Delilah.marinaL@gmail.com>
2025-09-26 14:21:37 -0400

    Async and OOP

--- oop/problem1-oop.js
+++ oop/problem1-oop.js
@@ -19,11 +19,43 @@
  * The methods that take an argument must accept only one argument, and it must be a string.
  */
 
+//success create a fun that creates an ob with new intances of bird, eeach bird should have 3 props species, color, location. attached to the protype should be 3 methods :getColor` (returns the color of the bird)
+//  * `setColor` (sets a new color for the bird)= input string
+//  * `getLocations` (returns the locations where the bird was found)
+//  * `addLocation` (adds a new location to the locations array) input string
+//  *
+
+function Bird(species, color, location) {
+  this.species = species;
+  this.color = color;
+  this.location = location;
+}
+
+Bird.prototype.setColor = function (color) {
+    this.color = color;
+};
+
+Bird.prototype.getColor = function () {
+  if (this.color) return this.color;
+
+  return "This information is unknown";
+};
+
+Bird.prototype.addLocation = function (newLocation) {
+  this.location.push(newLocation)
+
+};
+
+Bird.prototype.getLocations = function (newLocation) {
+  return this.location
+
+};
+
 // UNCOMMENT THESE LINES TO CHECK YOUR WORK
-// const newBird = new Bird('canary', 'red', ['newyork', 'spain']);
-// console.log(newBird); // => should log { species: 'canary', 'color: 'red', locations: ['newyork', 'spain'] }
-// console.log(newBird.getColor()); // => should return 'red'
-// newBird.setColor('yellow');
-// console.log(newBird.getColor()); // => should return 'yellow'
-// newBird.addLocation('france');
-// console.log(newBird.getLocations()); // => should return ['newyork', 'spain', 'france'];
+const newBird = new Bird("canary", "red", ["newyork", "spain"]);
+console.log(newBird); // => should log { species: 'canary', 'color: 'red', locations: ['newyork', 'spain'] }
+console.log(newBird.getColor()); // => should return 'red'
+newBird.setColor('yellow');
+console.log(newBird.getColor()); // => should return 'yellow'
+newBird.addLocation('france');
+console.log(newBird.getLocations()); // => should return ['newyork', 'spain', 'france'];
