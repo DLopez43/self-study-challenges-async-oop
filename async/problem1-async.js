c45079248dec977480c296ac1ee2ceee53068c0f
Delilah Lopez <Delilah.marinaL@gmail.com>
2025-09-26 14:42:16 -0400

    Async and OOP

--- async/problem1-async.js
+++ async/problem1-async.js
@@ -4,8 +4,20 @@
  * (in milliseconds) as arguments. It should return a promise that resolves
  * with the sum of the two numbers after the specified delay.
  */
-
+//success: a func with the label sumAfter Delay thatreturn a promise that will combine two inputs after a delay time once resolved
+//kw: create fun, inputs num1,num 2 and delay time, return  promise that sesolves with sum of num inputs
+//actions: create func, return promise, promise will resolve the sum of 2 inputs
+const sumAfterDelay=( num1, num2, waitTime )=> {
+    return new Promise((resolve,reject) => {
+        let sum = num1+num2;
+        if(typeof sum ==='number') {
+        setTimeout(()=>resolve(sum), waitTime)
+        } else{
+            reject('Something went wrong')
+        }
+    });
+}
 // UNCOMMENT THESE LINES TO CHECK YOUR WORK
-// sumAfterDelay(5, 10, 1000)
-//   .then((sum) => console.log(sum)) // should log 15 after 1 second
-//   .catch((error) => console.error('Error:', error));
+sumAfterDelay(5, 10, 1000)
+  .then((sum) => console.log(sum)) // should log 15 after 1 second
+  .catch((error) => console.error('Error:', error));
--- async/problem2-async.js
+++ async/problem2-async.js
@@ -4,8 +4,18 @@
  * It should return a promise that resolves with the string 'Data received'
  * after a random delay between 1 and 3 seconds.
  */
+//success: create a func simulateapicall that returns a promise that resolves with the string data received with a random delay from 1-3 secs
+//kw: creat a func, return a promise, promise will resolve with a string, with random seconds btwn 1-3
+//actions create func return promise, create set timeout with resolve string, create math.random for time delay
+
+const simulateAPICall = () =>{
+    return new Promise((resolve,reject) => {
+        let delay = (Math.floor(Math.random()*3)+1) * 1000
+        setTimeout(()=> resolve(`Data received`), delay);
+    });
+}
 
 // UNCOMMENT THESE LINES TO CHECK YOUR WORK
-// simulateAPICall()
-//   .then((message) => console.log(message)) // should log 'Data received' after a random delay between 1 and 3 seconds
-//   .catch((error) => console.error('Error:', error));
+simulateAPICall()
+  .then((message) => console.log(message)) // should log 'Data received' after a random delay between 1 and 3 seconds
+  .catch((error) => console.error('Error:', error));
