7ac04fc7c7efcf4f303ee8268d9c616bff25d1d8
Delilah Lopez <Delilah.marinaL@gmail.com>
2025-09-26 17:05:20 -0400

    refactor

--- oop/problem2-oop.js
+++ oop/problem2-oop.js
@@ -12,29 +12,46 @@
 //get title, get dir get release year = will display requested prop val in a string
 
 class Movie {
-    constructor(title, director, releaseYear) {
-        this.title = title;
-        this.director = director;
-        this.releaseYear = releaseYear; 
-    }
-    getTitle = function (){
+  constructor(title, director, releaseYear) {
+    this.title = title;
+    this.director = director;
+    this.releaseYear = releaseYear;
+  }
+  // getTitle = function (){
+  //     return this.title;
+  // }
+  // getDirector = function (){
+  //     return this.director;
+  // }
+  // getReleaseYear = function (){
+  //     return this.releaseYear;
+  // }
+  getInfo = function (info) {
+    switch (info) {
+      case "title":
         return this.title;
-    }
-    getDirector = function (){
-        return this.director;
-    }
-    getReleaseYear = function (){
-        return this.releaseYear;
-    }
-    getDetails = function (){
-        return `The movie ${this.title} directed by ${this.director}, released in ${this.releaseYear}`;
-    }
 
+      case "director":
+        return this.director;
+
+      case "year":
+        return this.releaseYear;
+
+      default:
+        return `That request doesn't exist`;
+    }
+  };
+  getDetails = function () {
+    return `The movie ${this.title} directed by ${this.director}, released in ${this.releaseYear}`;
+  };
 }
 
 // UNCOMMENT THESE LINES TO CHECK YOUR WORK
-const myMovie = new Movie('Inception', 'Christopher Nolan', 2010);
-console.log(myMovie.getTitle()); // => should return 'Inception'
-console.log(myMovie.getDirector()); // => should return 'Christopher Nolan'
-console.log(myMovie.getReleaseYear()); // => should return 2010
+const myMovie = new Movie("Inception", "Christopher Nolan", 2010);
+// console.log(myMovie.getTitle()); // => should return 'Inception'
+// console.log(myMovie.getDirector()); // => should return 'Christopher Nolan'
+// console.log(myMovie.getReleaseYear()); // => should return 2010
+console.log(myMovie.getInfo("title"));
+console.log(myMovie.getInfo("hi"));
+console.log(myMovie.getInfo("year"));
 console.log(myMovie.getDetails()); // => should return 'Inception directed by Christopher Nolan, released in 2010'
